d1c034d6c00238cc6d680fce89343881209ca74f
larysa <larisashcherbachenko@gmail.com>
2018-06-08 10:34:29 +0300

    Edit tests

--- tests/test_offer.py
+++ tests/test_offer.py
@@ -3,7 +3,7 @@ from jsonrpcclient.http_client import HTTPClient
 
 client = HTTPClient("http://127.0.0.1:8001/api/storage")
 
-data = {"cid":89, "buyer_addr":"1ba5ef234bb4e26a71a977958e074edf2924d81f",
+data = {"cid":89, "buyer_addr":"66ca783b8a37ea00d872559c612b5d6b650263e5",
 		"price":890}
 
 
